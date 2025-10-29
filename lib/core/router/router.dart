e1d4eb0070ab85cc7cf3679f1b74607a49dd54d7
jonghwan <s23061@gsm.hs.kr>
2025-10-29 16:08:19 +0900

    chore :: 누락된 로그인 라우터 추가

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
 import 'package:potato_4cut_v2/core/router/router_helper.dart';
+import 'package:potato_4cut_v2/features/login/presentation/pages/login_page.dart';
 
 final GlobalKey<NavigatorState> routerNavigationKey =
     GlobalKey<NavigatorState>();
 
 GoRouter router = GoRouter(
   navigatorKey: routerNavigationKey,
-  initialLocation: RoutePath.splash,
-  routes: [],
+  initialLocation: RoutePath.login,
+  routes: [
+    GoRoute(path: RoutePath.login, builder: (context, state) => LoginPage()),
+  ],
 );
