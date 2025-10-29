f459ac2f7c2e78bf1fc91b401c70d379d0727822
jonghwan <s23061@gsm.hs.kr>
2025-10-29 13:49:58 +0900

    style :: 스플래시 ui구현

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
 import 'package:potato_4cut_v2/core/router/router_helper.dart';
+import 'package:potato_4cut_v2/features/splash/presentation/pages/splash_page.dart';
 
 final GlobalKey<NavigatorState> routerNavigationKey =
     GlobalKey<NavigatorState>();
@@ -8,5 +9,10 @@ final GlobalKey<NavigatorState> routerNavigationKey =
 GoRouter router = GoRouter(
   navigatorKey: routerNavigationKey,
   initialLocation: RoutePath.splash,
-  routes: [],
+  routes: [
+    GoRoute(
+      path: RoutePath.splash,
+      builder: (context, state) => const SplashPage(),
+    ),
+  ],
 );
---
+++ lib/features/splash/presentation/pages/splash_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:potato_4cut_v2/core/theme/app_text_style.dart';
+import 'package:potato_4cut_v2/core/ui/default_scaffold.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultScaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset('assets/images/potato_4cut_logo.svg'),
+            SizedBox(height: 12.h),
+            Text('감자네컷', style: AppTextStyle.logoText),
+          ],
+        ),
+      ),
+    );
+  }
+}
