0ba6639b6c91d29fad036ddd8519d8ad285a7dd8
jonghwan <s23061@gsm.hs.kr>
2025-11-03 12:05:55 +0900

    feat :: API 통신을 위한 dio설정

---
+++ lib/core/network/dio.dart
@@ -0,0 +1,44 @@
+import 'package:dio/dio.dart';
+import 'package:dio/io.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+abstract class AppDio {
+  AppDio._internal();
+
+  static Dio? _instance;
+
+  static Dio getInstance() => _instance ??= _AppDio();
+}
+
+class _AppDio with DioMixin implements AppDio {
+  _AppDio() {
+    httpClientAdapter = IOHttpClientAdapter();
+    options = BaseOptions(
+      baseUrl: dotenv.env['apiBaseUrl']!,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      sendTimeout: const Duration(seconds: 30),
+      connectTimeout: const Duration(seconds: 30),
+      receiveTimeout: const Duration(seconds: 30),
+      receiveDataWhenStatusError: true,
+    );
+
+    interceptors.addAll(
+      [
+        InterceptorsWrapper(
+          onError: (error, handler) async {
+            return handler.reject(error);
+          },
+          onRequest: (options, handler) async {
+            return handler.next(options);
+          },
+        ),
+        LogInterceptor(
+          requestBody: true,
+          responseBody: true,
+        ),
+      ],
+    );
+  }
+}
