6a58bb750c3286a655d06bace3c0cdc0074f43a6
jonghwan <s23061@gsm.hs.kr>
2025-11-28 11:51:01 +0900

    fix :: 반환값 model -> entity로 변경

--- lib/features/login/domain/repositories/auth_repository.dart
+++ lib/features/login/domain/repositories/auth_repository.dart
@@ -1,27 +1,13 @@
-import 'package:potato_4cut_v2/features/login/data/models/login_response_dto.dart';
-
-class AuthResult {
-  final String accessToken;
-  final String refreshToken;
-  final LoginResponseDto user;
-  final bool newUser;
-
-  const AuthResult({
-    required this.accessToken,
-    required this.refreshToken,
-    required this.user,
-    required this.newUser,
-  });
-}
+import 'package:potato_4cut_v2/features/login/domain/entities/auth_result_entity.dart';
 
 abstract class AuthRepository {
-  Future<AuthResult> login({
+  Future<AuthResultEntity> login({
     required String provider,
     required String token,
     required String deviceToken,
   });
 
-  Future<AuthResult> refreshToken(String refreshToken);
+  Future<AuthResultEntity> refreshToken(String refreshToken);
 
   Future<void> logout();
 }
