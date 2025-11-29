718e83f37115291cc8b97153a4ca8f4d6ec6a0a1
jonghwan <s23061@gsm.hs.kr>
2025-11-29 21:25:00 +0900

    feat :: 임시 토큰 구현, 미동작

--- lib/features/take_photo/data/data_sources/photo_data_source_impl.dart
+++ lib/features/take_photo/data/data_sources/photo_data_source_impl.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http_parser/http_parser.dart';
 import 'package:dio/dio.dart';
 import 'package:potato_4cut_v2/core/network/dio.dart';
@@ -10,11 +11,14 @@ class PhotoDataSourceImpl implements PhotoDataSource {
   final Dio _dio;
 
   PhotoDataSourceImpl(Dio? dio) : _dio = dio ?? AppDio.getInstance();
-  final token =
-      "eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJkNWY1ZDA2OC0wOTdmLTQzMDYtOWVjNi1kNzI1OTM5ZmM5NDUiLCJyb2xlIjoiVVNFUiIsImlhdCI6MTc2NDI0MTE3MywiZXhwIjoxNzY0MjQ0NzczfQ.yjxHRZnYGDbPyyEl8kP51eNJYbFUbriqrW4XxATpR6YG6Vz-HDCZTOLseJSdy7xq";
+
+  final storage = FlutterSecureStorage();
+
+  
 
   @override
   Future<SavePhotosResponseModel> savePhotos(List<File> images) async {
+    final token = await storage.read(key: 'TOKEN');
     final List<MultipartFile> uploadFiles = [];
 
     for (File image in images) {
@@ -40,31 +44,31 @@ class PhotoDataSourceImpl implements PhotoDataSource {
 
   @override
   Future save4CutPhotos(File composedImage, List<String> photoCutIds) async {
-    // final List<MultipartFile> multipartFilePhotoCutIds = [];
-    // for (String photoCutId in photoCutIds) {
-    //   multipartFilePhotoCutIds.add(MultipartFile.fromString(photoCutId));
-    // }
-    // log('multipartFilePhotoCutIds = $multipartFilePhotoCutIds');
-
-    final MultipartFile file = await MultipartFile.fromFile(
+    final token = await storage.read(key: 'TOKEN');
+    final MultipartFile composedImageField = await MultipartFile.fromFile(
       composedImage.path,
       filename: composedImage.uri.pathSegments.last,
       contentType: MediaType("image", "jpeg"),
     );
+    final photoCutIdsField = MultipartFile.fromString(
+      jsonEncode(photoCutIds),
+      contentType: MediaType("application", "json"),
+    );
     final formData = FormData.fromMap({
-      'composedImage': file,
-      'photoCutIds': jsonEncode(photoCutIds),
+      'composedImage': composedImageField,
+      'photoCutIds': photoCutIdsField,
     });
 
     await _dio.post(
       '/api/v1/photos',
       data: formData,
-      options: Options(headers: {'Authorization': 'Bearer $token'}),
+      options: Options(headers: {"Authorization": "Bearer $token"}),
     );
   }
 
   @override
   Future importSpecificPhoto(String id) async {
+    final token = await storage.read(key: 'TOKEN');
     final response = await _dio.get(
       '/api/v1/photos/$id',
       options: Options(headers: {'Authorization': 'Bearer $token'}),
@@ -75,6 +79,7 @@ class PhotoDataSourceImpl implements PhotoDataSource {
 
   @override
   Future<void> deletePhoto(String id) async {
+    final token = await storage.read(key: 'TOKEN');
     await _dio.delete(
       '/api/v1/photos/$id',
       options: Options(headers: {'Authorization': 'Bearer $token'}),
