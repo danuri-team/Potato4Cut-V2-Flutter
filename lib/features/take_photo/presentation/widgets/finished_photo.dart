b2d3e40eb98c39b440070c75fd8f674c9edaecea
jonghwan <s23061@gsm.hs.kr>
2025-11-29 21:32:16 +0900

    feat :: RepaintBoundary위젯이 구현된 후 widget을 image로 변경

--- lib/features/take_photo/presentation/widgets/finished_photo.dart
+++ lib/features/take_photo/presentation/widgets/finished_photo.dart
@@ -1,18 +1,48 @@
+import 'package:flutter/scheduler.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:potato_4cut_v2/core/theme/app_color.dart';
 import 'package:potato_4cut_v2/core/theme/app_text_style.dart';
+import 'package:potato_4cut_v2/features/take_photo/provider/finished_photo_provider.dart';
 import 'package:potato_4cut_v2/features/take_photo/provider/photo_provider.dart';
 
-class FinishedPhoto extends ConsumerWidget {
+class FinishedPhoto extends ConsumerStatefulWidget {
   const FinishedPhoto({super.key, required this.repaintBoundaryKey});
 
   final GlobalKey repaintBoundaryKey;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<FinishedPhoto> createState() => _FinishedPhotoState();
+}
+
+class _FinishedPhotoState extends ConsumerState<FinishedPhoto> {
+  bool isGenerated = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _schedulePhotoGeneration();
+  }
+
+  void _schedulePhotoGeneration() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      SchedulerBinding.instance.addPostFrameCallback((_) {
+        SchedulerBinding.instance.addPostFrameCallback((_) {
+          if (mounted && !isGenerated) {
+            isGenerated = true;
+            ref
+                .read(finishedPhotoProvider.notifier)
+                .generateFinishedPhoto(widget.repaintBoundaryKey);
+          }
+        });
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final photos = ref.watch(photoProvider);
     return SizedBox(
       width: double.infinity,
@@ -39,7 +69,7 @@ class FinishedPhoto extends ConsumerWidget {
           ),
           Center(
             child: RepaintBoundary(
-              key: repaintBoundaryKey,
+              key: widget.repaintBoundaryKey,
               child: Container(
                 width: 242.w,
                 height: 412.h,
