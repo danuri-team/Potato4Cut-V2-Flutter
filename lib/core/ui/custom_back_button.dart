6988d00011db52bcccb720370aa24df6bbcafe4a
jonghwan <s23061@gsm.hs.kr>
2025-12-01 20:19:07 +0900

    fix :: 뒤로가기 버튼 onTap이 실행되지 않던 문제 해결

--- lib/core/ui/custom_back_button.dart
+++ lib/core/ui/custom_back_button.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:potato_4cut_v2/core/router/router_helper.dart';
+import 'package:potato_4cut_v2/core/util/throttle.dart';
 
 class CustomBackButton extends StatelessWidget {
-  const CustomBackButton({super.key});
+  const CustomBackButton({super.key, this.onTap});
+
+  final Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () => AppNavigation.pop(context),
+      onTap: () {
+        Throttle.run(() {
+          AppNavigation.pop(context);
+          onTap?.call();
+        });
+      },
       child: SvgPicture.asset('assets/images/chevron_left.svg'),
     );
   }
