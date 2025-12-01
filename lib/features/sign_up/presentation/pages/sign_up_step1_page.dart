c985ef3d80217918e6c2a8e6ce177eee6ccbdcc0
jonghwan <s23061@gsm.hs.kr>
2025-12-01 20:21:56 +0900

    feat :: 뒤로가기 시 회원가입 필드 상태 초기화

--- lib/features/sign_up/presentation/pages/sign_up_step1_page.dart
+++ lib/features/sign_up/presentation/pages/sign_up_step1_page.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:potato_4cut_v2/core/router/router_helper.dart';
 import 'package:potato_4cut_v2/core/theme/app_text_style.dart';
+import 'package:potato_4cut_v2/core/ui/custom_back_button.dart';
 import 'package:potato_4cut_v2/core/ui/default_layout.dart';
 import 'package:potato_4cut_v2/core/ui/submit_button.dart';
 import 'package:potato_4cut_v2/core/util/throttle.dart';
@@ -17,12 +17,15 @@ class SignUpStep1Page extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final signUpField = ref.watch(signUpFieldProvider);
-    final nickname = signUpField.nickname;
+
+    void goBack(WidgetRef ref) {
+      ref.read(signUpFieldProvider.notifier).resetField();
+    }
 
     return DefaultLayout(
       appBar: Padding(
         padding: EdgeInsets.only(left: 3.w),
-        child: SvgPicture.asset('assets/images/chevron_left.svg'),
+        child:  CustomBackButton(onTap: () => goBack(ref)),
       ),
       body: SingleChildScrollView(
         child: Padding(
@@ -30,7 +33,6 @@ class SignUpStep1Page extends ConsumerWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text('$nickname'),
               SizedBox(height: 48.h),
               Text('닉네임을 입력해주세요', style: AppTextStyle.heading1),
               SizedBox(height: 20.h),
@@ -41,7 +43,7 @@ class SignUpStep1Page extends ConsumerWidget {
                     Throttle.run(() => AppNavigation.goSignUpStep2(context)),
                 width: 343.w,
                 text: '확인',
-                isActivate: (nickname != null),
+                isActivate: (signUpField.nickname != null),
               ),
             ],
           ),
