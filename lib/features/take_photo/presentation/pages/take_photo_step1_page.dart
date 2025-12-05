e54bcb317a68d7062539ff00d7c79e5de6850b2f
jonghwan <s23061@gsm.hs.kr>
2025-12-05 14:29:39 +0900

    feat :: 뒤로가기 시 사진 관련 상태 초기화

--- lib/features/take_photo/presentation/pages/take_photo_step1_page.dart
+++ lib/features/take_photo/presentation/pages/take_photo_step1_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:potato_4cut_v2/core/enum/take_photo_flow_type.dart';
 import 'package:potato_4cut_v2/core/router/router_helper.dart';
 import 'package:potato_4cut_v2/core/theme/app_text_style.dart';
 import 'package:potato_4cut_v2/core/ui/custom_back_button.dart';
@@ -8,14 +10,29 @@ import 'package:potato_4cut_v2/core/ui/submit_button.dart';
 import 'package:potato_4cut_v2/core/util/throttle.dart';
 import 'package:potato_4cut_v2/features/take_photo/presentation/widgets/current_progress_indicator.dart';
 import 'package:potato_4cut_v2/features/take_photo/presentation/widgets/potato4cut_frame.dart';
+import 'package:potato_4cut_v2/features/take_photo/provider/current_page_index_provider.dart';
+import 'package:potato_4cut_v2/features/take_photo/provider/cut_ids_provider.dart';
+import 'package:potato_4cut_v2/features/take_photo/provider/finished_photo_provider.dart';
+import 'package:potato_4cut_v2/features/take_photo/provider/photo_provider.dart';
+import 'package:potato_4cut_v2/features/take_photo/provider/take_photo_flow_provider.dart';
 
-class TakePhotoStep1Page extends StatelessWidget {
+class TakePhotoStep1Page extends ConsumerWidget {
   const TakePhotoStep1Page({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return DefaultLayout(
-      appBar: const CustomBackButton(),
+      appBar: CustomBackButton(
+        onTap: () {
+          ref.read(photoProvider.notifier).reset();
+          ref.read(currentPageIndexProvider.notifier).update((state) => 0);
+          ref.read(cutIdsProvider.notifier).update((state) => []);
+          ref.read(finishedPhotoProvider.notifier).resetState();
+          ref
+              .read(takePhotoFlowProvider.notifier)
+              .update((state) => TakePhotoFlowType.TakePhoto);
+        },
+      ),
       body: Padding(
         padding: EdgeInsets.only(left: 16.w, right: 16.w, bottom: 16.h),
         child: Column(
