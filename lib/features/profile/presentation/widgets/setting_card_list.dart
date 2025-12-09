eb376c227c01b44adfc137db2b6d7e7ef2855ef7
jonghwan <s23061@gsm.hs.kr>
2025-12-09 09:07:06 +0900

    feat :: 개인정보 처리방침 url연결

--- lib/features/profile/presentation/widgets/setting_card_list.dart
+++ lib/features/profile/presentation/widgets/setting_card_list.dart
@@ -1,17 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:potato_4cut_v2/core/theme/app_color.dart';
 import 'package:potato_4cut_v2/core/theme/app_text_style.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class SettingCardList extends StatelessWidget {
+class SettingCardList extends StatefulWidget {
   const SettingCardList({super.key});
 
-  Widget settingCard({
-    required String text,
-    required Function() onTap,
-    Widget? suffixWidget,
-  }) {
+  @override
+  State<SettingCardList> createState() => _SettingCardListState();
+}
+
+class _SettingCardListState extends State<SettingCardList> {
+  Future<void> getPackageInfo() async {
+    final packageInfo = await PackageInfo.fromPlatform();
+    setState(() {
+      version = packageInfo.version;
+    });
+  }
+
+  String? version;
+
+  @override
+  void initState() {
+    super.initState();
+    getPackageInfo();
+  }
+
+  Widget card({required String text, Function()? onTap, Widget? suffixWidget}) {
     return GestureDetector(
       behavior: HitTestBehavior.opaque,
       onTap: onTap,
@@ -38,17 +56,25 @@ class SettingCardList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        settingCard(text: '언어 설정', onTap: () {}),
+        card(text: '언어 설정', onTap: () {}),
         SizedBox(height: 16.h),
-        settingCard(text: '개인정보 처리방침', onTap: () {}),
+        card(
+          text: '개인정보 처리방침',
+          onTap: () {
+            launchUrl(
+              Uri.parse(
+                'https://working-mailman-871.notion.site/28d5f20bda6f8023918cd8624f1c083d',
+              ),
+            );
+          },
+        ),
         SizedBox(height: 16.h),
-        settingCard(text: '이용 약관', onTap: () {}),
+        card(text: '이용 약관', onTap: () {}),
         SizedBox(height: 16.h),
-        settingCard(
+        card(
           text: '버전 정보',
-          onTap: () {},
           suffixWidget: Text(
-            'v0.1',
+            version ?? '',
             style: AppTextStyle.body1Normal.copyWith(
               fontWeight: FontWeight.w600,
               color: AppColor.label4,
