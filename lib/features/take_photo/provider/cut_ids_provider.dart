c9cb92fcd742fde44050d495c1e136a0f20d1566
jonghwan <s23061@gsm.hs.kr>
2025-12-17 20:03:16 +0900

    feat :: 사진 저장 API에 사용되는 필드 상태 구현

--- lib/features/take_photo/provider/cut_ids_provider.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final cutIdsProvider = StateProvider<List<String>>((ref) => []);
---
+++ lib/features/take_photo/provider/object_key_provider.dart
@@ -0,0 +1,3 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final objectKeyProvider = StateProvider<String?>((ref) => null,);
