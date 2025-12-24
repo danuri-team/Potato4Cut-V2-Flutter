4a18a09cbb22c4c6e397c8f92b870157698a1aa0
jonghwan <s23061@gsm.hs.kr>
2025-12-24 15:26:24 +0900

    fix :: refresh토큰 만료시간 검증식 수정

---
+++ lib/presentation/login/providers/stoarage_provider.dart
@@ -0,0 +1,45 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:potato_4cut_v2/core/router/router_helper.dart';
+import 'package:potato_4cut_v2/core/storage/token_storage.dart';
+import 'package:potato_4cut_v2/presentation/sign_up/providers/auth_state.dart';
+
+final storageProvider =
+    StateNotifierProvider<StoarageNotifier, FlutterSecureStorage>(
+      (ref) => StoarageNotifier(),
+    );
+
+class StoarageNotifier extends StateNotifier<FlutterSecureStorage> {
+  StoarageNotifier() : super(const FlutterSecureStorage());
+
+  final storageKey = 'authStatus';
+
+  Future<void> checkAuthAndNavigate(BuildContext context) async {
+    final authStatusString = await state.read(key: storageKey);
+    final tokenStorage = TokenStorage();
+    final tokenExpiration = await tokenStorage.checkTokenExpiration();
+
+    if(tokenExpiration){
+      AppNavigation.goLogin(context);
+      return;
+    }
+
+    if(!context.mounted) return;
+
+    if (authStatusString == null || authStatusString == AuthStatus.unauthenticated.toString()) {
+        AppNavigation.goLogin(context);
+    } else if (authStatusString == AuthStatus.authenticated.toString()) {
+        AppNavigation.goHome(context);
+    }
+  }
+
+  Future<void> login() async{
+    await state.write(key: storageKey, value: AuthStatus.authenticated.toString());
+  }
+
+  Future<void> logout() async{
+    await state.write(key: storageKey, value: AuthStatus.unauthenticated.toString());
+  }
+}
